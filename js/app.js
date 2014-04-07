89cbcf802cc4eebf92d4d687cee9706375301ba5
Caresa <unimportantfact@gmail.com>
2014-04-06 21:57:46 -0500

    added cursor to purchase element remove hard code

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
-	$(".purchase").click(function(){
+	$("#addItem").keyup(function(event){
+		if (event.which == 13) {
+			newItem = $(this).val();
+			$("#food").append('<div class="purchase"><span><i class="icon-remove-circle"></i>'+newItem+'</span></div>');
+			addItemClick();
+			$(this).val("");
+		}
+	});
+	addItemClick();
+});
+
+function addItemClick(){
+	$(".purchase").unbind("click");
+	$(".purchase").on('click', function(){
 		if ($(this).hasClass("complete")){
 			$(this).removeClass("complete").animate({left:'0px'}).find("i").hide();
 		}
@@ -7,4 +20,8 @@ $(document).ready(function(){
 			$(this).addClass("complete").animate({left:'150px'}).find("i").show();
 		}
 	});
-});
+	$('.icon-remove-circle').unbind('click');
+	$(".icon-remove-circle").on('click', function(){
+		$(this).closest(".purchase").remove();
+	});
+}
